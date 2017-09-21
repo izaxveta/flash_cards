ddf74dcf5a900c7169392c6c6b1de057797dc4ab
Iza Jimenez <izavetha@gmail.com>
2017-09-20 21:58:35 -0600

    feedback and correct? methods added

---
+++ lib/guess.rb
@@ -0,0 +1,18 @@
+class Guess
+  attr_reader :response,
+              :card
+  def initialize(response, card)
+    @response = response
+    @card = card
+    @feedback = feedback
+  end
+
+  def correct?
+    response == card.answer
+  end
+
+  def feedback
+    "Correct!" if correct? == true
+    "Incorrect!" if correct? == false
+  end
+end
