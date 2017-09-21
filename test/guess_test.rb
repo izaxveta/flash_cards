655c81a0ce73e325d957bda289ecccd03726d1de
Iza Jimenez <izavetha@gmail.com>
2017-09-20 22:00:45 -0600

    correct? and feedback method tests passing

---
+++ test/guess_test.rb
@@ -0,0 +1,49 @@
+require './test/test_helper'
+
+class GuessTest < Minitest::Test
+  attr_reader :card,
+              :guess
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau")
+    @guess = Guess.new("Juneau", card)
+  end
+
+  def test_guess_exists
+    assert_instance_of Guess, guess
+  end
+
+  def test_card_exists
+    assert_instance_of Card, card
+  end
+
+  def test_it_has_a_response
+    assert_equal "Juneau", guess.response
+  end
+
+  def test_it_has_a_card
+    assert_equal card, guess.card
+    assert_instance_of Card, guess.card
+  end
+
+  def test_guess_correct_returns_true_if_response_matches_card_answer
+    assert guess.correct?
+  end
+
+  def test_guess_correct_returns_false_if_response_does_not_match_answer
+    card = Card.new("What is the capital of Alaska?", "Juneau")
+    guess = Guess.new("Nebraska", card)
+
+    refute guess.correct?
+  end
+
+  def test_guess_feedback_returns_correct_when_response_is_correct
+    assert "Correct!", guess.feedback
+  end
+
+  def test_guess_feedback_returns_incorrect_when_response_is_incorrect
+    card = Card.new("What is the capital of Alaska?", "Juneau")
+    guess = Guess.new("Nebraska", card)
+
+    assert_equal "Incorrect!", guess.feedback
+  end
+end
